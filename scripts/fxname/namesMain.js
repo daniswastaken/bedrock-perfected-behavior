01b2e8e5589349e843debe4a49562cef4e629582
daniswastaken <daniswastaken@gmail.com>
2025-09-06 06:45:11 +0700

    organizing scripts

---
+++ scripts/fxname/namesMain.js
@@ -0,0 +1,26 @@
+import { world } from "@minecraft/server";
+import humanNameList from "./array/humanNames.js";
+import fantasyNames from "./array/fantasyNames.js";
+
+const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+export function registerMobNaming() {
+  world.afterEvents.entitySpawn.subscribe(event => {
+    const mob = event.entity;
+    if (!mob) return;
+
+    switch (mob.typeId) {
+      case "minecraft:villager_v2":
+        mob.nameTag = pickRandom(humanNameList);
+        break;
+
+      case "minecraft:wandering_trader":
+        mob.nameTag = pickRandom(humanNameList);
+        break;
+
+      case "minecraft:iron_golem":
+        mob.nameTag = pickRandom(fantasyNames);
+        break;
+    }
+  });
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,3 @@
-import { world } from "@minecraft/server";
-import humanNameList from "./array/humanNames.js";
-import fantasyNames from "./array/fantasyNames.js";
+import { registerMobNaming } from "./fxname/namesMain.js";
 
-const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)];
-
-world.afterEvents.entitySpawn.subscribe(event => {
-  const mob = event.entity;
-
-  if (!mob) return;
-
-  switch (mob.typeId) {
-    case "minecraft:villager_v2":
-      mob.nameTag = pickRandom(humanNameList);
-      break;
-
-    case "minecraft:wandering_trader":
-      mob.nameTag = pickRandom(humanNameList);
-      break;
-
-    case "minecraft:iron_golem":
-      mob.nameTag = pickRandom(fantasyNames);
-      break;
-  }
-});
+registerMobNaming();
