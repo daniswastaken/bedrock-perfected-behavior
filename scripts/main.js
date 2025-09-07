892da897daad46d738b1f9bc194af7d90ba3c37c
daniswastaken <daniswastaken@gmail.com>
2025-09-07 13:06:31 +0700

    damage indicator

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import registerMobNaming from "./fxname/namesMain.js";
 import coordinateCompass from "./coordinateCompass/coordinateCompassMain.js";
+import damageMain from "./damage/damageMain.js";
 
 registerMobNaming();
 coordinateCompass();
+damageMain();
