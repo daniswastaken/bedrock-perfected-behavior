2b025ac42ac06835461a9dc986ef6a851b0a9d12
daniswastaken <daniswastaken@gmail.com>
2025-09-20 13:57:04 +0700

    removing experiment from main

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 import registerMobNaming from "./fxname/namesMain.js";
 import coordinateCompass from "./coordinateCompass/coordinateCompassMain.js";
 import damageMain from "./damage/damageMain.js";
-import hidePlayer from "./labs/hide_player.js"
-
-
 
 registerMobNaming();
 coordinateCompass();
