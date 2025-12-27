0ba3823e607a73cf7a39ebd63ec94f84b765e627
daniswastaken <daniswastaken@gmail.com>
2025-12-27 22:23:24 +0700

    removal of biomeEntry

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 import registerMobNaming from "./fxname/namesMain.js";
 import coordinateCompass from "./coordinateCompass/coordinateCompassMain.js";
 import damageMain from "./damage/damageMain.js";
-import biomentry from "./biomentry/biomentryMain.js";
-
 
 import raycast from "./labs/raycast.js";
 
